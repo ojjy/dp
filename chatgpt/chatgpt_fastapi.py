83112419f2308d23a968b7af9fb1f629a5f2b5e3
ojjy <ojjy90@gmail.com>
2023-05-03 18:14:56 +0900

    add

---
+++ chatgpt/chatgpt_fastapi.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+import openai
+import os
+import uvicorn
+from starlette.responses import FileResponse
+import json
+
+# Initialize OpenAI API credentials
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize FastAPI app
+app = FastAPI()
+
+# Define root endpoint for the chatbot
+@app.get("/")
+def form():
+    return FileResponse('index.html')
+
+
+# Define endpoint for processing user input and returning chatbot response
+@app.post("/chat")
+async def chat(query: str = Form(...)):
+    # Generate response from OpenAI API
+    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    print(project_path)
+    with open(os.path.join(project_path, "secret.json"), 'r') as jp:
+        json_contents = json.loads(jp.read())
+    openai.api_key = json_contents['chatgpt_apikey']
+    response = openai.ChatCompletion.create(
+        engine="gpt-3.5-turbo",
+        prompt=f"Conversation with the chatbot:\n\nUser: {query}\nBot:",
+        temperature=0.7,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract text from OpenAI API response
+    message = response.choices[0].text.strip()
+
+    # Return chatbot response
+    return HTMLResponse(content=f"<html><body><p>Chatbot: {message}</p></body></html>")
+
+
+if __name__ =="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
